d2d7016380a25fa020c36b69cce4c75df44294f5
mikelele <mikelele@mikepc.localdomain>
2022-08-28 14:30:06 +0300

    added arguments of command line

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 #include "functions.hpp"
 
 
-int main()
+int main(int argc, char* argv[])
 {
    double a = 0.0, b = 0.0, c = 0.0;
    double firstRoot = 0.0, secondRoot = 0.0;
    enum equationSolveType definedEquationSolveType = {};
 
-   printf("Enter 3 coefficients such as a, b, c to solve quadratic equation:\n"); 
+   //printf("Enter Solve to solve quadratic equation or Test to launch unit tests\n");
 
-   makeDataEntryCorrect(&a, &b, &c);
-   solveEquation(a, b, c, &definedEquationSolveType, &firstRoot, &secondRoot);
-   printResultsOfSolvedEquation(definedEquationSolveType, firstRoot, secondRoot);
-       
-   //unitTests();
+   if (argc != 2)
+   {
+        printf("You entered not correct numbers of arguments\n");
+        return 0;
+   }
+
+   if (!strcmp(argv[1], "Solve"))
+   {
+        printf("Enter 3 coefficients such as a, b, c to solve quadratic equation:\n"); 
+        makeDataEntryCorrect(&a, &b, &c);
+        solveEquation(a, b, c, &definedEquationSolveType, &firstRoot, &secondRoot);
+        printResultsOfSolvedEquation(definedEquationSolveType, firstRoot, secondRoot);
+   }
+   else if (!strcmp(argv[1], "Test"))
+   {
+        unitTests();
+   }
+   else
+   {
+        printf("You entered not correct command");
+   }   
 
    return 0;
 }
